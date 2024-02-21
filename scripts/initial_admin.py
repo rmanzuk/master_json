c8e829a6fe9bf4463df42e782a9dabfddc8aff80
Ryan <rmanzuk@princeton.edu>
2024-02-20 20:35:36 -0500

    added lines for getting more data

--- scripts/initial_admin.py
+++ scripts/initial_admin.py
@@ -1,6 +1,6 @@
 # script to perform initial admin tasks on an outcrop json and sample jsons
 # written by R. A. Manzuk 02/14/2024
-# last updated 02/14/2024
+# last updated 02/19/2024
 
 ##########################################################################################
 # package imports
@@ -14,7 +14,7 @@ import json # for json handling
 # local function imports
 ##########################################################################################
 # %% 
-from json_processing import assemble_samples
+from json_processing import assemble_samples, select_gridded_geochem, select_gridded_im_metrics, select_gridded_point_counts
 # %% 
 ##########################################################################################
 # script lines
@@ -32,4 +32,9 @@ with open(outcrop_json_file, 'r') as f:
 
 outcrop_data = assemble_samples(outcrop_data, sample_json_dir, data_type=['grid_data'], data_name=["Stewart's Mill Grid"])
 
-    
+# %% select the gridded geochem data, im metrics, and point counts
+geochem_df = select_gridded_geochem(outcrop_data)
+
+im_metric_df =  select_gridded_im_metrics(outcrop_data)
+
+point_count_df = select_gridded_point_counts(outcrop_data)
